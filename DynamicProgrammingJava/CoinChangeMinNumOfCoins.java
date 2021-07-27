528912d3adac309b216625757426557fcf0e482f
Ishita <53152779+Ishita-0112@users.noreply.github.com>
2021-07-27 21:15:10 +0530

    Update CoinChangeMinNumOfCoins.java

--- DynamicProgrammingJava/CoinChangeMinNumOfCoins.java
+++ DynamicProgrammingJava/CoinChangeMinNumOfCoins.java
@@ -1,31 +1,16 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int[][] memo = new int[n+1][amount+1];
-        for (int i=0;i<n+1;i++){
-            for (int j=0;j<amount+1;j++){
-                if (i==0)
-                    memo[i][j] = Integer.MAX_VALUE;
-                if (j==0)
-                    memo[i][j] = 0;
-                
-            }
-        }
-        for (int j=1;j<amount+1;j++){
-            if (j%coins[0]==0)
-                memo[1][j] = j/coins[0];
-            else
-                memo[1][j] = Integer.MAX_VALUE;
-        }
-        for (int i=1;i<n+1;i++){
-            for (int j=1;j<amount+1;j++){
-                if (coins[i-1]<=j)
-                    memo[i][j] = Math.min(memo[i][j-coins[i-1]]+1, memo[i-1][j]);
-                else
-                    memo[i][j] = memo[i-1][j];
-            }
-        }
-        return memo[n][amount] == Integer.MAX_VALUE ? -1 : memo[n][amount];
-        
+        //dp array in size of total amount
+        int[] dp=new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++)
+            for(int c:coins)
+                //if curr amount greater than curr coin type
+                if(i>=c) 
+                    //then update the dp value with min of curr dp value and dp[currAmt-currCoin] + 1
+                    dp[i]=Math.min(dp[i],dp[i-c]+1);
+        //if no combination then return -1
+        return dp[amount]>amount?-1:dp[amount];
     }
 }
