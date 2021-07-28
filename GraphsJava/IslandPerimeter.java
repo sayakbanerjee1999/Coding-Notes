b62d7039803e0322435dd8b39fd8dfea9b16cb83
Ishita <53152779+Ishita-0112@users.noreply.github.com>
2021-07-29 00:47:20 +0530

    Update IslandPerimeter.java

--- GraphsJava/IslandPerimeter.java
+++ GraphsJava/IslandPerimeter.java
@@ -1,3 +1,21 @@
+/*
+You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+Output: 16
+Explanation: The perimeter is the 16 yellow stripes in the image above.
+*/
+
+
 class Solution {
     int count  = 0;
     public int islandPerimeter(int[][] grid) {
