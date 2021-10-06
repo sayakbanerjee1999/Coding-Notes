9e90024e9ab088a7ef014f18fcf84b9f8e7b2ea3
Sayak Banerjee <43917478+sayakbanerjee1999@users.noreply.github.com>
2021-10-06 10:19:23 +0530

    Updated LCS

--- C++/Dynamic Programming/lcs.cpp
+++ C++/Dynamic Programming/lcs.cpp
@@ -6,21 +6,21 @@ public:
         int m = text1.length();
         int n = text2.length();
         
-        int arr[m + 1][n + 1];
+        int dp[m + 1][n + 1];
         
         for(int i = 0; i <= m; i++){
             for(int j = 0; j <= n; j++){
                 if(i==0 || j==0)
-                    arr[i][j] = 0;
+                    dp[i][j] = 0;
                 
                 else if(text1[i-1] == text2[j-1])
-                    arr[i][j] = arr[i - 1][j - 1] + 1;
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 
                 else
-                    arr[i][j] = max(arr[i - 1][j], arr[i][j - 1]);
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
         
-        return arr[m][n];
+        return dp[m][n];
     }
 };
