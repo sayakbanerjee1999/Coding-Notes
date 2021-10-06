2e8bdc6e40c1164d8f5f249c3a778fa6722d26bc
Sayak Banerjee <43917478+sayakbanerjee1999@users.noreply.github.com>
2021-10-06 10:11:25 +0530

    Added CoinChange1

---
+++ C++/Dynamic Programming/coinChange1.cpp
@@ -0,0 +1,32 @@
+Coin Change I - Minimum Number of Ways
+
+class Solution
+{
+  public:
+    long long int count( int S[], int m, int n )
+    {
+        long long int dp[m+1][n+1];
+        
+        for(int i = 0; i < m+1; i++){
+            for(int j = 0; j < n+1; j++){
+                if(i == 0)
+                    dp[i][j] = 0;
+                if(j == 0)
+                    dp[i][j] = 1;
+            }
+        }
+        
+        
+        for(int i = 1; i < m+1; i++){
+            for(int j = 1; j < n+1; j++){
+                if(S[i-1] <= j)
+                    dp[i][j] = dp[i-1][j] + dp[i][j-S[i-1]];
+                
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
