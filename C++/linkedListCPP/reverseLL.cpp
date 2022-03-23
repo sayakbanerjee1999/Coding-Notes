1287b134220def8dca44d8495c9b3b6302e4e838
Sayak Banerjee <43917478+sayakbanerjee1999@users.noreply.github.com>
2022-03-23 20:14:53 +0530

    Added ReverseLL

---
+++ C++/linkedListCPP/reverseLL.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* n = NULL;
+        ListNode* prev = NULL;
+        
+        while(curr != NULL){
+            n = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = n;
+        }
+        
+        return prev;
+    }
+};
