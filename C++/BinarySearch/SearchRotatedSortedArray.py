37587f7bc7671e0292de1c0ea22ebc9068437279
Sayak Banerjee <43917478+sayakbanerjee1999@users.noreply.github.com>
2025-08-20 12:45:08 -0400

    Create SearchRotatedSortedArray.py

---
+++ C++/BinarySearch/SearchRotatedSortedArray.py
@@ -0,0 +1,29 @@
+class Solution:
+    def search(self, arr: List[int], target: int) -> int:
+        lo = 0
+        hi = len(arr) - 1
+
+        ans = -1
+
+        while(lo <= hi):
+            mid = lo + (hi - lo) // 2
+
+            if arr[mid] == target:
+                ans = mid
+
+            # Left Half Sorted
+            if arr[lo] <= arr[mid]:
+                if target >= arr[lo] and target <= arr[mid]:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+
+            else:
+                if target <= arr[hi] and target >= arr[mid]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+        
+        return ans
+
+            
