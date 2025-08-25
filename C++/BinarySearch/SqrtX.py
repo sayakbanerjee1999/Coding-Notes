d7ddceb121f0d11dd03dc40f8e4b17337b0191d2
Sayak Banerjee <43917478+sayakbanerjee1999@users.noreply.github.com>
2025-08-24 23:34:39 -0400

    Create SqrtX.py

---
+++ C++/BinarySearch/SqrtX.py
@@ -0,0 +1,32 @@
+class Solution:
+    def helper(self, x: int, mid: int) -> int:
+        if mid * mid == x:
+            return 1
+        elif mid * mid < x:
+            return 2
+        else:
+            return 3 
+
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+            
+        lo = 1
+        hi = x
+
+        ans = -1
+
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+
+            if self.helper(x, mid) == 1:
+                ans = mid
+                break
+            # If mid element smaller than eleminate left half and search only in right-half (But assign answer as well since smallest integer is needed)
+            elif self.helper(x, mid) == 2:
+                ans = mid
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        
+        return ans
