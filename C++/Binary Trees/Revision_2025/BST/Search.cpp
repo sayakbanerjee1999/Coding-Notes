4b2eae09a8ce50ef683f20c8b27d12021e3442c0
Sayak Banerjee <43917478+sayakbanerjee1999@users.noreply.github.com>
2025-09-29 19:03:23 -0400

    Create Search.cpp

---
+++ C++/Binary Trees/Revision_2025/BST/Search.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode* curr = root;
+        while(curr){
+            if (curr->val == val)
+                return curr;
+            else{
+                if(val > curr->val)
+                    curr = curr->right;
+                else
+                    curr = curr->left;
+            }
+        }
+
+        return NULL;
+    }
+};
