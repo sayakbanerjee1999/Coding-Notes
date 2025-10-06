366374215185e5edf060feac488ba6055b3738c0
Sayak Banerjee <43917478+sayakbanerjee1999@users.noreply.github.com>
2025-10-06 17:57:31 -0400

    Update MergeSortLL.cpp

--- C++/linkedListCPP/MergeSortLL.cpp
+++ C++/linkedListCPP/MergeSortLL.cpp
@@ -1,47 +1,60 @@
-Node* merge2LL(Node* list1, Node* list2) {
-    // Create a dummy node to serve
-    // as the head of the merged list
-    Node* dummyNode = new Node(-1);
-    Node* temp = dummyNode;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* findMiddle(ListNode* head){
+        ListNode* slowPtr = head;
+        ListNode* fastPtr = head->next;
 
-    // Traverse both lists simultaneously
-    while (list1 != nullptr && list2 != nullptr) {
-        // Compare elements of both lists and
-        // link the smaller node to the merged list
-        if (list1->data <= list2->data) {
-            temp->next = list1;
-            list1 = list1->next;
-        } else {
-            temp->next = list2;
-            list2 = list2->next;
+        while(fastPtr && fastPtr->next){
+            fastPtr = fastPtr->next->next;
+            slowPtr = slowPtr->next;
         }
-        // Move the temporary pointer
-        // to the next node
-        temp = temp->next; 
+
+        return slowPtr;
     }
 
-    // If any list still has remaining
-    // elements, append them to the merged list
-    if (list1 != nullptr) {
-        temp->next = list1;
-    } else {
-        temp->next = list2;
+    ListNode* merge2LL(ListNode* first, ListNode* second){
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+
+        while(first && second){
+            if(first->val <= second->val){
+                temp->next = first;
+                first = first->next;
+            }
+            else{
+                temp->next = second;
+                second = second->next;
+            }
+            temp = temp->next;
+        }
+
+        if(first) temp->next = first;
+        else temp->next = second;
+
+        return dummy->next;
     }
-    // Return the merged list starting 
-    // from the next of the dummy node
-    return dummyNode->next;
-}
 
-Node* sortLL(Node* head){
-    if(head == NULL || head->next == NULL) return head;
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
   
-    Node* middle = findMiddle(head);       // Use fastPtr slowPtr; slowPtr = head; fastPtr = head->next
-    Node* right = middle->next;
-    middle->next = NULL;
-    Node* first = head;
-  
-    Node* first = sortLL(first);
-    Node* second = sortLL(right);
+        ListNode* middle = findMiddle(head);       // Use fastPtr slowPtr; slowPtr = head; fastPtr = head->next
+        ListNode* right = middle->next;
+        middle->next = NULL;
+        ListNode* first = head;
+    
+        ListNode* left_ = sortList(first);
+        ListNode* right_ = sortList(right);
 
-    return merge2LL(first, second);
-}
+        return merge2LL(left_, right_);
+    }
+};
