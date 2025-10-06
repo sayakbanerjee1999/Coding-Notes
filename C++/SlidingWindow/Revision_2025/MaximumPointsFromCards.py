aa3eb77999399870ef49c8ca59336687ef7eeb22
Sayak Banerjee <43917478+sayakbanerjee1999@users.noreply.github.com>
2025-10-06 00:42:24 -0400

    Update MaximumPointsFromCards.py

--- C++/SlidingWindow/Revision_2025/MaximumPointsFromCards.py
+++ C++/SlidingWindow/Revision_2025/MaximumPointsFromCards.py
@@ -1,14 +1,16 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
         sum_ = sum(x for x in cardPoints[:k])
-        maxsum = sum_
+        maxSum_ = sum_
 
-        idx = len(cardPoints)-1
+        idx = len(cardPoints) - 1
         for i in range(k-1, -1, -1):
+            # Subtract from Begging
             sum_ -= cardPoints[i]
+            # Add from the End
             sum_ += cardPoints[idx]
 
-            maxsum = max(maxsum, sum_)
-            idx = idx - 1
-
-        return maxsum
+            idx -= 1
+            maxSum_ = max(maxSum_, sum_)
+        
+        return maxSum_
