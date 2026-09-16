52f0c1779ebb5b0731b6cfb32a3627b3058160df
Sayak Banerjee <43917478+sayakbanerjee1999@users.noreply.github.com>
2026-09-15 22:26:52 -0400

    Add Possible_Bipartition solution in Python
    Implement a solution to determine if a graph can be bipartitioned based on dislikes.

---
+++ C++/GraphsCPP/Possible_Bipartition.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        
+        # Create the Adjacency List
+        adj = defaultdict(list)
+        for person_a, person_b in dislikes:
+            adj[person_a].append(person_b)
+            adj[person_b].append(person_a)
+
+        # (n+1) -> since 1-indexed
+        color = [-1] * (n + 1)
+        flag = 0
+
+        def dfs(src, par, col):
+            nonlocal flag
+
+            color[src] = col
+
+            for nei in adj[src]:
+                # If not colored -> color with opposite color
+                if color[nei] == -1:
+                    dfs(nei, src, 3 - col)
+                # If it is already colored
+                # If this is not my parent and my neighbor has the same color as me, we found a conflict.
+                # elif nei != par and color[nei] == col:
+                # The parent condition to check is actually redundant but its common practice to check
+                elif color[nei] == col:
+                    flag = 1
+                    return
+
+        # Start from different vertices
+        for i in range(1, n + 1):
+            if color[i] == -1:
+                dfs(i, -1, 1)
+
+        return flag == 0
+        
