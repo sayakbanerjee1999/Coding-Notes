26ef2e23e9d1d60767894655dc064972de4c1566
Sayak Banerjee <43917478+sayakbanerjee1999@users.noreply.github.com>
2026-09-25 19:10:42 -0400

    Add exclusiveTime method to calculate function durations
    Implement exclusive time calculation for functions based on logs.

---
+++ C++/LKND/Exclusive_Function_Time.py
@@ -0,0 +1,48 @@
+
+# The fundamental difference
+# start
+
+# A start timestamp marks the beginning of a new function.
+# Therefore, the previous function's interval is:
+# [prevTime, timestamp)
+# So:
+# timestamp - prevTime
+
+# end
+
+# An end timestamp marks the last time unit occupied by the function.
+# Therefore, the function's interval is:
+# [prevTime, timestamp]
+# So:
+# timestamp - prevTime + 1
+
+# That's the entire reason for the +1.
+
+class Solution:
+  def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+      prevTime = 0
+      stack = []
+      function_time = [0] * n
+
+      for log in logs:
+          function_id, type_, timestamp = log.split(":")
+          function_id = int(function_id)
+          timestamp = int(timestamp)
+
+          if type_ == "start":
+              if stack:
+                  function_executing = stack[-1]
+                  function_time[function_executing] += (timestamp - prevTime)
+
+              stack.append(function_id)
+              prevTime = timestamp
+
+          else: 
+              function_executing = stack.pop()
+              function_time[function_executing] += (timestamp - prevTime + 1)
+
+              # When a function ends it has consumed the whole unit of time. 
+              # Next job can only start from the next unit of time.
+              prevTime = timestamp+1
+      
+      return function_time
